509c1e609da1567723599c5230d45f22a6ae6f01
chaselancaster <chaseblancaster@gmail.com>
2019-03-19 16:54:08 -0700

    Commit 9 - Created loops.js file

---
+++ loop-questions/app.js
@@ -0,0 +1,37 @@
+/*for (let i = 0; i <= 99; i++) {
+    console.log(i);
+};*/
+
+/*for (let i = 99; i >= 0; i--) {
+    console.log(i);
+};*/
+
+/*for (let i = 298; i >= 1; i -= 3) {
+    console.log(i);
+};*/
+
+/*for (let i = 0; i <= 98; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}*/
+
+/*for (let i = 98; i >= 0; i--) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+};*/
+
+/*for (let i = 49; i <= 72; i++) {
+    console.log(i);
+};*/
+
+/*for (let i = 81; i >= 26; i--) {
+    console.log(i);
+};*/
+
+for (let i = 1; i <= 96; i++) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+};
